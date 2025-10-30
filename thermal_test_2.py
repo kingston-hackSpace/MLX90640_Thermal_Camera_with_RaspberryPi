d5268640b846175ee03aa9f51a213d34b2c4ab2c
Camila Colussi <c.colussi@kingston.ac.uk>
2025-10-30 13:16:15 +0000

    update

--- thermal_test_2.py
+++ thermal_test_2.py
@@ -7,37 +7,54 @@ import matplotlib.pyplot as plt
 
 i2c = busio.I2C(board.SCL, board.SDA)
 mlx = adafruit_mlx90640.MLX90640(i2c)
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ  # Set to a feasible refresh rate
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ  # Set to a feasible refresh rate
 
 plt.ion()
-fig, ax = plt.subplots(figsize=(12, 7))
-therm1 = ax.imshow(np.zeros((24, 32)), vmin=0, vmax=60)
-cbar = fig.colorbar(therm1)
-cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14)
+fig, ax = plt.subplots(figsize=(8, 6))
+therm1 = ax.imshow(np.zeros((24, 32)), vmin=20, vmax=30, interpolation='bilinear', cmap='inferno')
 
-frame = np.zeros((24*32,))
+cbar = fig.colorbar(therm1)
+cbar.set_label('Temperature [$^{\circ}$C]', fontsize=12)
+
+frame = np.zeros((24*32,), dtype=np.float32)
+previous_frame = np.zeros((24, 32), dtype=np.float32)
+alpha = 0.5
 t_array = []
 max_retries = 5
 
+#main loop
 while True:
     t1 = time.monotonic()
+    last_exception = None
     retry_count = 0
+    
     while retry_count < max_retries:
         try:
-            mlx.getFrame(frame)
-            data_array = np.reshape(frame, (24, 32))
-            therm1.set_data(np.fliplr(data_array))
-            therm1.set_clim(vmin=np.min(data_array), vmax=np.max(data_array))
+            mlx.getFrame(frame) #read sensor
+            new_frame = frame.reshape((24, 32))
+            
+            #smooth image for faster view
+            smoothed_frame = alpha * new_frame + (1 - alpha) * previous_frame
+            previous_frame = smoothed_frame.copy()
+            
+            #update the image
+            therm1.set_data(np.flipud(np.fliplr(smoothed_frame)))
+            therm1.set_clim(vmin=np.min(smoothed_frame), vmax=np.max(smoothed_frame))
             fig.canvas.draw()  # Redraw the figure to update the plot and colorbar
             fig.canvas.flush_events()
             plt.pause(0.001)
+            
+            #update the sample rate
             t_array.append(time.monotonic() - t1)
-            print('Sample Rate: {0:2.1f}fps'.format(len(t_array)/np.sum(t_array)))
+            fps = len(t_array) / np.sum(t_array)
+            print("Sample Rate: {fps.1f}fps")
             break
-        except ValueError:
+            
+        except (ValueError, RuntimeError) as e:
+            last_exception = e
             retry_count += 1
-        except RuntimeError as e:
-            retry_count += 1
-            if retry_count >= max_retries:
-                print(f"Failed after {max_retries} retries with error: {e}")
-                break
+            time.sleep(0.05)
+            
+    else:
+                print(f"Failed after {max_retries} retries with error: {last_exception}")
+                
